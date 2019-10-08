76e42003725099a4268d2c8aaaf46fa5009d3650
Jakub Tomanik <j.tomanik@gmail.com>
2019-10-08 22:39:52 +0900

    Added image shift every 2 sec

--- demo.py
+++ demo.py
@@ -1,3 +1,4 @@
+import time
 import os.path
 from PIL import Image
 from luma.core.interface.serial import i2c
@@ -8,16 +9,29 @@ from luma.oled.device import ssd1327
 def main():
     serial = i2c(port=0, address=0x3C)
     device = ssd1327(serial, 128, 128)
-    img_path = os.path.abspath(os.path.join(os.path.dirname(__file__),'images', 'oled.network.png'))
-    qr_code = Image.open(img_path).convert("RGBA").rotate(180)
 
-    background = Image.new("RGBA", device.size, "black")
-    posn = ((device.width - qr_code.width) // 2, 0)
+    images = [
+        "oled.qr.png",
+        "oled.network.png",
+        "oled.hardware.png"
+    ]
 
-    background.paste(qr_code, posn)
-    device.display(background.convert(device.mode))
-
+    counter = 0
     while True:
+        number = len(images)
+        file = images[counter % number]
+
+        img_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'images', file))
+        image = Image.open(img_path).convert("RGBA").rotate(180)
+
+        background = Image.new("RGBA", device.size, "black")
+        posn = ((device.width - image.width) // 2, 0)
+
+        background.paste(image, posn)
+        device.display(background.convert(device.mode))
+        time.sleep(2)
+
+        counter = counter +1
         continue
 
 
