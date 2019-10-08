48a8718d38d529036f4b8aac33bf13e02695ed3a
Jakub Tomanik <j.tomanik@gmail.com>
2019-10-08 22:14:50 +0900

    Initial commit

---
+++ demo.py
@@ -0,0 +1,26 @@
+import os.path
+from PIL import Image
+from luma.core.interface.serial import i2c
+from luma.core.render import canvas
+from luma.oled.device import ssd1327
+
+
+def main():
+    serial = i2c(port=0, address=0x3C)
+    device = ssd1327(serial, 128, 128)
+    img_path = os.path.abspath(os.path.join(os.path.dirname(__file__),'images', 'out.png'))
+    qr_code = Image.open(img_path).convert("RGBA")
+
+    background = Image.new("RGBA", device.size, "black")
+    posn = ((device.width - qr_code.width) // 2, 0)
+
+    while True:
+        background.paste(qr_code, posn)
+        device.display(background.convert(device.mode))
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
